1f65aa86978e553d55db9a723f6118bfb8b83c5c
mariasolegallina <gallinamariasole@gmail.com>
2024-04-29 16:52:48 +0200

    add - laravel-one-to-many

---
+++ app/Http/Controllers/Admin/TypeController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreTypeRequest;
+use App\Http\Requests\UpdateTypeRequest;
+use App\Models\Type;
+
+use function PHPSTORM_META\type;
+
+class TypeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $types = Type::all();
+
+        return view('admin.types.index', compact('types'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTypeRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Type $type)
+    {
+        dd($type->projects);
+
+        return view('admin.type.show', compact('type'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Type $type)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTypeRequest $request, Type $type)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Type $type)
+    {
+        //
+    }
+}
